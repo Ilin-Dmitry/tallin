1e817d277ee56b1eef8fc76f64f4416bebee6371
Ilin-Dmitry <dm-ilin@mail.ru>
2023-04-19 19:06:08 +0400

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    filename: path.resolve(__dirname, 'src/index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name][contenthash].js',
+    assetModuleFilename: 'assets/[name][ext]',
+    clean: true
+  },
+  performance: {
+    hints: false,
+    maxAssetSize: 512000,
+    maxEntrypointSize: 512000
+  },
+  devServer: {
+    port: 9000,
+    compress: true,
+    hot: true,
+    static: {
+      directory: path.join(__dirname, 'dist')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
+      },
+    ]
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      title: 'Tallin',
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ]
+}
